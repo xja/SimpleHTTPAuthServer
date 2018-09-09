65d9c39b3120af3e30a9f4cb5fa733d14663c41a
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-09-09 18:32:42 -0400

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@
 from SimpleHTTPAuthServer import __version__, __prog__
 from distutils.core import setup
 
-setup(name=__prog__,
-      version=__version__,
-      description='Simple Auth Server SSL',
-      author='Michael Li',
-      author_email='@tianhuil',
-      url='https://github.com/tianhuil/SimpleHTTPAuthServer/',
-      packages=[__prog__],
-     )
+setup(
+	name=__prog__,
+	version=__version__,
+	description='Simple HTTP and HTTPS Auth Server',
+	author='Michael Li',
+	author_email='@tianhuil',
+	url='https://github.com/tianhuil/SimpleHTTPAuthServer/',
+	packages=[__prog__],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
